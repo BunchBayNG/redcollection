5b827f9272d3ba7002508eb4ff16893138da1327
MENTOR <odiichibueze7@gmail.com>
2025-07-02 10:20:18 +0100

    chore: code refactor:

--- src/main/java/com/bbng/dao/microservices/vacctgen/config/SearchFilter.java
+++ src/main/java/com/bbng/dao/microservices/vacctgen/config/SearchFilter.java
@@ -1,29 +1,22 @@
-//package com.bbng.dao.microservices.vacctgen.config;
-//
-//import lombok.Value;
-//import lombok.experimental.NonFinal;
-//import org.springframework.format.annotation.DateTimeFormat;
-//
-//import java.math.BigDecimal;
-//import java.util.Date;
-//
-//@Value
-//@NonFinal
-//public class SearchFilter {
-//    private String accountNo;
-//    private String initiatorRef;
-//    private BigDecimal amount;
-//    private String accountName;
-//
-//    @DateTimeFormat(
-//            pattern = "dd/MM/yyyy HH:mm:ss",
-//            fallbackPatterns = {"dd/MM/yyyy HH:mm", "dd/MM/yyyy"}
-//    )
-//    private Date fromDate;
-//
-//    @DateTimeFormat(
-//            pattern = "dd/MM/yyyy HH:mm:ss",
-//            fallbackPatterns = {"dd/MM/yyyy HH:mm", "dd/MM/yyyy"}
-//    )
-//    private Date toDate;
-//}
+package com.bbng.dao.microservices.vacctgen.config;
+
+import lombok.Value;
+import lombok.experimental.NonFinal;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Value
+@NonFinal
+public class SearchFilter {
+    private String accountNo;
+    private String initiatorRef;
+    private BigDecimal amount;
+    private String accountName;
+
+    private LocalDateTime fromDate;
+
+    private LocalDateTime toDate;
+}
